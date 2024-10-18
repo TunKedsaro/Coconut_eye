b33442a9c14ca1a4071c8be9f09edad5f6c7769b
Tun <tunkedsaro@gmail.com>
2024-10-18 15:41:34 +0700

    Model loaded

--- libtest.py
+++ libtest.py
@@ -6,3 +6,136 @@ import numpy as np
 from skimage import transform as trans
 from keras.models import load_model
 import pickle
+
+
+class FaceModel:
+    def __init__(self, embedding_model_path):
+        self.session = ort.InferenceSession(embedding_model_path)
+
+    def preprocess_image(self, img_input):
+        # Check if the input is a file path (string) or a NumPy array (image)
+        if isinstance(img_input, str):  # If it's a file path
+            img = cv2.imread(img_input)
+            if img is None:
+                raise ValueError(f"Image not found at path: {img_input}")
+        elif isinstance(img_input, np.ndarray):  # If it's an image (NumPy array)
+            img = img_input
+        else:
+            raise TypeError("Input must be a file path (str) or an image (np.ndarray)")
+
+        # Resize the image to (112, 112)
+        img = cv2.resize(img, (112, 112))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = np.transpose(img, (2, 0, 1)).astype(np.float32)
+        img = img / 255.0
+        img = np.expand_dims(img, axis=0)  # Add batch dimension
+        return img
+
+    def get_embedding(self, img):
+        inputs = self.session.get_inputs()
+        outputs = self.session.run(None, {inputs[0].name: img})
+        embedding = outputs[0]
+        return embedding[0]
+class FacePreprocessor:
+    def __init__(self,image_size='112,112',margin=44):
+        self.image_size = [int(x) for x in image_size.split(',')]
+        if len(self.image_size) == 1:
+            self.image_size = [self.image_size[0],self.image_size[0]]
+        self.margin = margin
+        assert len(self.image_size) == 2
+        assert self.image_size[0] == 112 and (self.image_size[1] == 112 or self.image_size[1] == 96)
+    def read_image(self,img_path,mode='rgb',layout='HWC'):
+        if mode == 'gray': # gray -> gray
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        else:
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            if mode == 'rgb':
+                img = img[..., ::-1]
+            if layout == 'CHW':
+                img = np.transpose(img,(2,0,1))
+        return img
+    def preprocess(self, img, bbox=None, landmark=None):
+        if isinstance(img, str):
+            img = self.read_image(img)
+
+        M = None
+        if landmark is not None:
+            assert len(self.image_size) == 2
+            src = np.array([
+                [30.2946, 51.6963],
+                [65.5318, 51.5014],
+                [48.0252, 71.7366],
+                [33.5493, 92.3655],
+                [62.7299, 92.2041]], dtype=np.float32)
+
+            if self.image_size[1] == 112:
+                src[:, 0] += 8.0
+            dst = landmark.astype(np.float32)
+
+            tform = trans.SimilarityTransform()
+            tform.estimate(dst, src)
+            M = tform.params[0:2, :]
+
+        if M is None:
+            return self._center_crop(img, bbox)
+        else:
+            return self._warp_image(img, M)
+
+    def _center_crop(self, img, bbox):
+        if bbox is None:
+            det = np.zeros(4, dtype=np.int32)
+            det[0] = int(img.shape[1] * 0.0625)
+            det[1] = int(img.shape[0] * 0.0625)
+            det[2] = img.shape[1] - det[0]
+            det[3] = img.shape[0] - det[1]
+        else:
+            det = bbox
+
+        bb = np.zeros(4, dtype=np.int32)
+        bb[0] = np.maximum(det[0] - self.margin // 2, 0)
+        bb[1] = np.maximum(det[1] - self.margin // 2, 0)
+        bb[2] = np.minimum(det[2] + self.margin // 2, img.shape[1])
+        bb[3] = np.minimum(det[3] + self.margin // 2, img.shape[0])
+
+        ret = img[bb[1]:bb[3], bb[0]:bb[2], :]
+        if len(self.image_size) > 0:
+            ret = cv2.resize(ret, (self.image_size[1], self.image_size[0]))
+        return ret
+
+    def _warp_image(self, img, M):
+        assert len(self.image_size) == 2
+        warped = cv2.warpAffine(img, M, (self.image_size[1], self.image_size[0]), borderValue=0.0)
+        return warped
+
+def findCosineDistance(vector1, vector2):
+    vec1 = vector1.flatten()
+    vec2 = vector2.flatten()
+
+    a = np.dot(vec1.T,vec2)
+    b = np.dot(vec1.T,vec1)
+    c = np.dot(vec2.T,vec2)
+
+    return 1 - (a/(np.sqrt(b)*np.sqrt(c)))
+def CosineSimilarity(test_vec, source_vecs):
+    cos_dist = 0
+    for source_vec in source_vecs:
+        cos_dist += findCosineDistance(test_vec, source_vec)
+    return cos_dist/len(source_vecs)
+
+embedding_model_path = r"C:\Users\Acer\Desktop\FaceRecAPI\models\w600k_r50.onnx"
+face_model = FaceModel(embedding_model_path)
+detector = MTCNN()
+preprocessor = FacePreprocessor(image_size='112,112',margin=44)
+# Load the classifier model
+mymodel = r"C:\Users\Acer\Desktop\FaceRecAPI\models\normal_model.h5"
+model = load_model(mymodel)
+# Load label
+embeddings = r"C:\Users\Acer\Desktop\FaceRecAPI\models\embeddings_datasets.pickle"
+le = r"C:\Users\Acer\Desktop\FaceRecAPI\models\le.pickle"
+with open(embeddings,"rb") as f:
+    data = pickle.load(f)
+with open(le,"rb") as f:
+    le = pickle.load(f)
+embeddings = np.array(data['embeddings'])
+labels = le.fit_transform(data['names'])         # แทนชื่อคนด้วยตัวเลข
+print(labels)
