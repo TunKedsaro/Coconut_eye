02e20011ba6158bc9234dfaa0048a3134f4700f4
Tun <tunkedsaro@gmail.com>
2024-11-23 10:54:34 +0700

    Open img,onnx,pickle.h5

--- main4.py
+++ main4.py
@@ -8,9 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle 
 # from keras.models import load_model
+import time
 
-
-print("Import done!!!")
+print("Import library done!")
 
 # Connect S3 with key (Got it's from ...)
 s3 = boto3.client(
@@ -22,46 +22,70 @@ s3 = boto3.client(
 
 bucket_name = "model-test-211124-0920"
 
-# # image
-# response = s3.get_object(
-#     Bucket = bucket_name,
-#     Key = "ComfyUI_00016_.png"
-# )
-# img_bytes = response['Body'].read()
+st = time.time()
+# image
+response = s3.get_object(
+    Bucket = bucket_name,
+    Key = "ComfyUI_00016_.png"
+)
+img_bytes = response['Body'].read()
 
-# img_array = np.frombuffer(img_bytes, np.uint8)
-# img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-# img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_array = np.frombuffer(img_bytes, np.uint8)
+img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # plt.imshow(img_rgb)
 # plt.show()
-
-# # embeddings_datasets.pickle
-# response = s3.get_object(
-#     Bucket = bucket_name,
-#     Key = "embeddings_datasets.pickle"
-# )
-# model_bytes = response['Body'].read()
-# obj = pickle.loads(model_bytes)
-# print(obj)
-
-# # le.pickle
-# response = s3.get_object(
-#     Bucket = bucket_name,
-#     Key = "le.pickle"
-# )
-# le_pickle_bytes = response['Body'].read()
-# obj = pickle.loads(le_pickle_bytes)
-# print(obj)
+et = time.time()
+print("Import image done",round(et-st,5))
 
 
+# embeddings_datasets.pickle
+st = time.time()
+response = s3.get_object(
+    Bucket = bucket_name,
+    Key = "embeddings_datasets.pickle"
+)
+model_bytes = response['Body'].read()
+obj = pickle.loads(model_bytes)
+et = time.time()
+print("import pickle done",round(et-st,5))
 
-# normal_model.h5
-
+# le.pickle
+st = time.time()
+response = s3.get_object(
+    Bucket = bucket_name,
+    Key = "le.pickle"
+)
+le_pickle_bytes = response['Body'].read()
+obj = pickle.loads(le_pickle_bytes)
+et = time.time()
+print("import pickle done",round(et-st,5))
 
 # w600k_r50.onnx
+st = time.time()
 response = s3.get_object(
     Bucket = bucket_name,
     Key = "w600k_r50.onnx"
 )
 face_embedding_Bytes = response['Body'].read()
 face_embedding = ort.InferenceSession(face_embedding_Bytes)
+et = time.time()
+print("Import .onnx done",round(et-st,5))
+
+# normal_model.h5 I
+import boto3
+import io
+import h5py
+
+st = time.time()
+response = s3.get_object(
+    Bucket = bucket_name,
+    Key = "normal_model.h5"
+)
+normal_model_byte = response['Body'].read()
+normal_model_Bytes_Binary = io.BytesIO(normal_model_byte)
+face_embedding = h5py.File(normal_model_Bytes_Binary,'r')
+et = time.time()
+print("import h5done!",round(et-st,5))
+
+# print(face_embedding.summary())
